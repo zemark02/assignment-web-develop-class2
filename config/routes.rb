dd872574a02f27b82e5cb6efff48fe9c7ba31d65
zemark02 <60037933+zemark02@users.noreply.github.com>
2021-08-28 16:45:01 +0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "somchai",to: "main#xxx"
   get "welcome", to: "main#welcome"
-  get "contact" , to: "main#contact"
+  get "contact_us" , to: "main#contact"
 end
