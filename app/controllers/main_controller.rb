5b2054a8f0f202a50513555a876864a260357030
mark <makhawanz@hotmail.com>
2021-09-06 14:19:28 +0700

    assignment2

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,5 +9,30 @@ class MainController < ApplicationController
   end
 
   def contact
+    a = params[:id]
+    @u = User.find(a)
+
+    
+  end
+
+  def create
+    message = params[:msg]
+    userid = params[:user_id]
+    if !User.find_by(id:userid)
+      User.create(id:userid)
+    end
+    Post.create(user_id:userid,msg:message)
+  end
+
+  def read
+
+    userid = params[:user_id]
+    a = User.find(userid)
+    @s = ""
+    co = 1
+    for i in User.find(userid).posts do
+      @s = "Post #{co} : #{i.msg} \n\n"
+      co = co+1
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   get 'somchai/xxx'
 
   get 'main/welcome'
-  get 'main/contact'
+  get 'main/contact_us'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "somchai",to: "main#xxx"
+  get "welcome", to: "main#welcome"
+  get "contact_us" , to: "main#contact"
+  get "post/create" ,to: "main#create"
+  get "user/read" , to: "main#read"
 end
